96f46e9103aac10ce314bc422add234f7cca4e80
unknown <simonyan-junior@yandex.ru>
2024-06-25 12:50:01 +0300

    fix

--- app/src/main/java/com/example/testprojectusecase/presentation/MainActivity.kt
+++ app/src/main/java/com/example/testprojectusecase/presentation/MainActivity.kt
@@ -14,6 +14,7 @@ import com.example.testprojectusecase.R
 
 class MainActivity : AppCompatActivity() {
 
+
     private val userRepository by lazy(LazyThreadSafetyMode.NONE) {
         UserRepositoryImpl(SharedPrefUserStorage(applicationContext))
     }
